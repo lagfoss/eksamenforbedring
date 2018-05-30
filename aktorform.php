973081a2b5ed9b586723a3ea9a977f4ce233b2b2
Tca91 <tca47@live.dk>
2018-05-30 11:41:32 +0200

    php aktør igen

--- aktorform.php
+++ aktorform.php
@@ -2,16 +2,17 @@
 
 if (isset($_POST['submit'])){
   $name = $_POST['navn'];
-  $subject = $_POST['besked'];
+  $subject = $_POST['beskrivelse'];
+  $subject2 = $_POST['artikel'];
   $mailFrom = $_POST['email'];
 
 
 
   $mailTo = "lagfoss@gmail.com";
   $headers = "From: ".$mailFrom;
-  $txt = "Du har modtaget en mail fra ".$name.".\n\n".$subject;
+  $txt = "Du har modtaget en mail fra ".$name.".\n\n".$subject.".\n\n".$subject2;
 
-  mail($mailTo, $subject, $txt, $headers);
+  mail($mailTo, $subject, $subject2, $txt, $headers);
   header("Location:send.html?mailsend");
 }
 
